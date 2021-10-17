cf911cc0029730bb139fceed4e0dd4e46e0838c0
Jon Mikel Merino Ruiz <merino.100363@e.unavarra.es>
2021-10-17 19:38:23 +0200

    + update stringCalculator

--- katas/src/stringCalculator.ts
+++ katas/src/stringCalculator.ts
@@ -2,5 +2,8 @@ export const stringCalculator = (numbers: string) => {
     if (numbers.length === 0) {
         return 0
     }
+    if (numbers.length === 1) {
+        return 1
+    }
     return 1
 }
